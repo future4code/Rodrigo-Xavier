b126b692ba6a2bb158ac5139e0164638d6879376
Rodrigo Xavier <rodrfx@gmail.com>
2020-09-29 16:56:32 -0300

    exercicios, aula operadores

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,69 @@
+// **Exercícios de interpretação de código**
+// 1.
+// a. false
+// b. false
+// c. true
+// e. boolean
+
+// 2.
+// a. undefined
+// b. null
+// c. 11
+// d. 3
+// e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// f. 9
+
+// **Exercícios de escrita de código**
+// 1.
+// a)
+// const idade = prompt("Qual é a sua idade?")
+// // b)
+// const  idadeAmigo = prompt("Qual é a sua idade do seu melhor amigo?")
+// // c)
+// const maior = Number(idade) > Number(idadeAmigo)
+// console.log("Sua idade é maior do que a do seu melhor amigo?", maior)
+// // d)
+// const diferenca = Number(idade) - Number(idadeAmigo)
+// console.log("Sua idade é maior do que a do seu melhor amigo?", diferenca)
+
+// 2.
+// a)
+// const par = prompt("Insira um número par")
+// // b)
+// const restoDivisao = Number(par) % 2
+// console.log(restoDivisao)
+// // c)
+// Sempre tem o resto igual a 0
+// d)
+// Sempre tem resto é igual a 1
+
+// 3.
+// a)
+// let listaDeTarefas = []
+// // b)
+// let tarefa1 = prompt("Tarefa 1/3")
+// let tarefa2 = prompt("Tarefa 2/3")
+// let tarefa3 = prompt("Tarefa 3/3")
+// listaDeTarefas.push(tarefa1)
+// listaDeTarefas.push(tarefa2)
+// listaDeTarefas.push(tarefa3)
+// // c)
+// console.log(listaDeTarefas)
+// // d)
+// const tarefaRealizada = prompt("Qual tarefa você já realizou 0, 1 ou 2?")
+// console.log(tarefaRealizada)
+// // e)
+// listaDeTarefas.splice(Number(tarefaRealizada),1)
+// // f)
+// console.log(listaDeTarefas)
+
+// 4)
+// const nome = prompt("Qual seu nome?")
+// const email = prompt("Qual seu e-mail?")
+// console.log("O e-mail " + email + " foi cadastrado com sucesso. Seja bem-vindo(a), " + nome)
+
+
+
+
+
+
