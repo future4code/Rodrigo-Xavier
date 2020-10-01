90bff2b1596056570790ef9226dd73d8861b2c2d
Rodrigo Xavier <rodrfx@gmail.com>
2020-10-01 16:50:56 -0300

    exercicios loop

---
+++ semana3/loops/js-vanilla-template/src/index.js
@@ -0,0 +1,35 @@
+// EXERCICIOS DE INTERPRETACAO DE CODIGO
+
+// 1. um laço até o número 4, 10 
+// 2. a. os numeros maiores que 18
+//    b. Sim, so mudar o numero para para > de 0, ou deletar a linha if
+
+// EXERCICIOS DE ESCRITA DE CODIGO
+// 3.
+// a.
+// let arrayOriginal = [10, 20, 30, 40, 50, 60]
+// for(let numero of arrayOriginal ){
+//     console.log(numero)
+// }
+
+// b.
+
+// let arrayOriginal = [10, 20, 30, 40, 50, 60]
+// for(let numero of arrayOriginal ){
+//     console.log(numero/10)
+// }
+
+// c.
+// let arrayOriginal = [1, 2, 3, 4, 5, 6]
+// let novoArray = []
+
+// for(i = 0; i < arrayOriginal.length; i++) {
+//     if(arrayOriginal[i] % 2 === 0){
+//         novoArray.push(arrayOriginal[i])
+//     }
+// }
+// console.log(novoArray)
+
+// d.
+
+
