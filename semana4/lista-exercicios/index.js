1d30d15b3deb0c8d6619fada7ee86cbeb7aa2a82
Rodrigo Xavier <rodrfx@gmail.com>
2020-10-09 12:08:59 -0300

    lista de exercicios

---
+++ semana4/lista-exercicios/index.js
@@ -0,0 +1,8 @@
+// EXERCICIO DE INTERPRETACAO DE CODIGO// 1. Temos uma função com um parametro valorEmDolar, a constante cotacao pede ao usuario a cotacao do dia, e o return multiplica o parametro da funcao com a cotacao que o usuario informou, depois a constante guarda a informaçao do meuDinheiro e invoca a funçao conversorDeMoeda, o console.log() imprimi no console.
+
+//2. A função tem dois parametros, tipo de investimento e valor, ela verifica qual investimento a pessoa quer fazer e multiplica pelo valor do tipo de investimento, a constante novoMontante irá imprimir o valor de 150 * 1.1, já o segundoMontante irá apresentar o alerta de tipo de investimento informado incorreto.
+
+//3. Temos um array numeros que vários números pares e ímpares, rodamos um for para varrer este array, se o numero for par ele será colocado dentro da variavel array1, se for impar ele será colocado dentro do array2.
+//O primeiro console.log irá apresentar a mensagem Quantidade total de numeros e a quantidade de numeros, o console.log 2 ira apresentar a quantidade de numeros pares do array1, e o console.log 3 irá apresentar os numeros impares do array2.
+
+//4. Temos uma constante com um array nomeado de numeros, duas variaveis com numero 1 e numero 2, no laço todos os numeros sao testados, o numero 1 é infinito sempre será maior que qualquer número, no if é testado se o numero é maior que o infinito, sempre será menor entao o numero 1, sera o numero, na segunda verificação pegamos o numermo e verificamos se ele é maior que 0 ele nao irá pegar o numero -10.
