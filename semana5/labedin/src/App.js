3e71b5f701e9dc7208c05edda295d688b31aa04c
Rodrigo Xavier <rodrfx@gmail.com>
2020-10-14 22:22:36 -0300

    exercicio aula 18- componenetes + props

--- semana5/labedin/src/App.js
+++ semana5/labedin/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
+import CardPequeno from './components/CardPequeno/CardPequeno';
 import ImagemButton from './components/ImagemButton/ImagemButton';
 import perfil from './Imagem/foto-perfil.jpg'
+import fotoExperiencia1 from './Imagem/experiencia1.png'
+import fotoExperiencia2 from './Imagem/experiencia2.png'
+import email from './Imagem/email.jpg'
+import localizacao from './Imagem/localizacao.png'
 
 function App() {
   return (
@@ -10,7 +15,7 @@ function App() {
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
         <CardGrande
-          imagem={perfil}
+          imagem={perfil} 
           nome="Rodrigo" 
           descricao="Oi, eu sou o Rodrigo. Tenho 35 anos e sou de Uberlândia - Minas Gerais"
         />
@@ -21,18 +26,31 @@ function App() {
         />
       </div>
 
+      <div className="container-email-endereco">
+        <CardPequeno
+          imagem={email}
+          email="E-mail: rodrfx@gmail.com"
+        />
+        <br/>
+        <CardPequeno
+          imagem={localizacao}
+          endereco= "Endereço: Rua Antônio Rezende de Chaves, 1016"
+        />
+      </div>
+
+
       <div className="page-section-container">
         <h2>Experiências profissionais</h2>
         <CardGrande 
-          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
-          nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+          imagem={fotoExperiencia1}
+          nome="Labenrisus imperdietu" 
+          descricao="Sed eu enim commodo risus imperdiet placerat." 
         />
         
         <CardGrande 
-          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
+          imagem={fotoExperiencia2}
+          nome="Aliquam rutrum" 
+          descricao="Proin ex lectus, mollis ut nisl sed, sollicitudin tempus magna." 
         />
       </div>
 
