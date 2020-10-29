465a84952ce2e178ea4babb7e70dd617bb966a48
Rodrigo Xavier <rodrfx@gmail.com>
2020-10-29 17:45:47 -0300

    fim da resolucao

--- semana7/apis-na-natureza/src/App.js
+++ semana7/apis-na-natureza/src/App.js
@@ -5,37 +5,38 @@ import axios from 'axios';
 class App extends React.Component {
 
   state = {
-    activityData: ''
+    activityData: null
 
   }
 
-  //funcao que e executada qdo o botao é clicado
-  onClickButton = () => {
+  componentDidMount(){
+    this.getActivity()
+  }
+  
+  getActivity = () => {
     axios.get('http://www.boredapi.com/api/activity/').then((response) => {
     this.setState({activityData: response.data}) //atualizando o estado, com os dados do .data
-    
     })
   };
-  
   
   render(){
     console.log('Atividade: ', this.state.activity)
   return (
     <div>
-      <button onClick={this.onClickButton}>Próxima atividade</button>
-      {}
-
-      <div>
-        <p>Nome:{this.state.activityData.activity} </p>
-        <p>Tipo: </p>
-        <p>Número de participantes: </p>
-        <p>Preço: </p>
-
+      <button onClick={this.getActivity}>Próxima atividade</button>
+      {this.state.activityData && <div>
+        <p>Nome: {this.state.activityData.activity} </p>
+        <p>Tipo: {this.state.activityData.type} </p>
+        <p>Número de participantes: {this.state.activityData.participants}</p>
+        <p>Preço: {this.state.activityData.price}</p>
       </div>  
-
-    </div>
+      }
+    
+  </div>
   );
 };
 }
+
+
 
 export default App;
