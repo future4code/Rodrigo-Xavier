d2aaef586395b08243ab6a9db9999480f7a58276
Rodrigo Xavier <rodrfx@gmail.com>
2020-11-16 16:32:34 -0300

    iniciando projeto labex

---
+++ semana10/labex/src/ components/ApplicationFormPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function  ApplicationFormPage() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default  ApplicationFormPage;
+
---
+++ semana10/labex/src/ components/HomePage.js
@@ -0,0 +1,14 @@
+import Axios from 'axios';
+import React from 'react';
+
+function  HomePage() {
+  return (
+    <div>
+        HomePage
+    </div>
+  )
+}
+
+
+
+export default  HomePage;
