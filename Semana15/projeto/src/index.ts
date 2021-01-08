3d282130b4024a2bda9414ddb91a47e918d92b88
Rodrigo Xavier <rodrfx@gmail.com>
2021-01-08 16:36:57 -0300

    entrega-projeto-f4Bank

--- Semana15/projeto/src/index.ts
+++ Semana15/projeto/src/index.ts
@@ -1,5 +1,5 @@
 //importando express com Request e Response e cors
-import express, {Request, Response} from 'express';
+import express, {Request, Response, Express} from 'express';
 import cors from 'cors';
 
 //extra: importando configuração de rede do node
@@ -13,58 +13,82 @@ app.use(cors());
 
 type user = {
     id: number,
-    name: string,
-    email: string,
-    type: string,
-    age: number
+    nome: string,
+    cpf: number,
+    conta: TipoConta,
+    transacao: TipoTransacao,
+    dataNascimento: number,
+    saldo: number
+
 }
 
+enum TipoConta {
+    CONTACORRENTE = "Conta-Corrente",
+    CONTAPOUPANCA = "Conta-Poupança"
+  }
+
+  enum TipoTransacao {
+    TED = "TED",
+    PIX = "PIX",
+    BOLETOBANCARIO = "Boleto-Bancário"
+  }
 
 let users: user[] = [
     {
         id: 1,
-        name: "Alice",
-        email: "alice@email.com",
-        type: "ADMIN",
-        age: 12
+        nome: "Ana",
+        cpf: 10110112302,
+        conta: TipoConta.CONTAPOUPANCA,
+        transacao: TipoTransacao.PIX,
+        dataNascimento: 1990,
+        saldo: 0,
+
     },
     {
         id: 2,
-        name: "Bob",
-        email: "bob@email.com",
-        type: "NORMAL",
-        age: 36
-    },
-    {
-        id: 3,
-        name: "Coragem",
-        email: "coragem@email.com",
-        type: "NORMAL",
-        age: 21
-    },
-    {
-        id: 4,
-        name: "Dory",
-        email: "dory@email.com",
-        type: "NORMAL",
-        age: 17
-    },
-    {
-        id: 5,
-        name: "Elsa",
-        email: "elsa@email.com",
-        type: "ADMIN",
-        age: 17
-    },
-    {
-        id: 6,
-        name: "Fred",
-        email: "fred@email.com",
-        type: "ADMIN",
-        age: 60
+        nome: "João",
+        cpf: 10110112305,
+        conta: TipoConta.CONTACORRENTE,
+        transacao: TipoTransacao.BOLETOBANCARIO,
+        dataNascimento: 1975,
+        saldo: 0,
+
     }
 ]
 
+app.get("/user/all", (req: Request, res: Response) => {
+    res.status(200).send(users)
+})
+
+app.post("/user", (req: Request, res: Response) => {
+
+    let errorCode: number = 400;
+
+    try {
+        const reqBody: user = {
+            id: Date.now(),
+            nome: req.body.nome,
+            cpf: req.body.cpf,
+            conta: req.body.conta,
+            transacao: req.body.transacao,
+            dataNascimento: req.body.dataNascimento,
+            saldo: req.body.saldo
+        }
+
+        if(!reqBody.nome || !reqBody.cpf || !reqBody.dataNascimento){
+            errorCode = 422;
+            throw new Error("Algum campo está inválido. Preencha corretamente.");
+        }
+
+        users.push(reqBody);
+    
+        res.status(200).send({message: "Usuário inserido com sucesso!"});
+        
+    } catch (error) {
+        res.status(errorCode).send({message: error.message});
+    }
+
+});
 
 
 
